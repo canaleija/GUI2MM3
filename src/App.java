3f698c4219b57ee09edd9e2dd0929ce28b483435
canaleija <robanac@gmail.com>
2024-06-03 11:19:26 -0600

    first commit

---
+++ src/App.java
@@ -0,0 +1,23 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+import escuchadores.EscuchadorBoton;
+import gui.FrameCombo;
+import gui.FrameFormulario;
+import herencia.Alumno;
+import herencia.Persona;
+
+import java.awt.GridLayout;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+     
+    //  Combo Box  =  "JComboBox"  Constructores(4) (api oracle) | Object,Component, Container, J
+    //  estado del combo cambie (selección de lo elementos que tiene) 
+    FrameCombo aux = new FrameCombo();
+        
+    }
+}

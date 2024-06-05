95e59afd0319e1ad04d65adcc2af626ddbadd571
canaleija <robanac@gmail.com>
2024-06-05 07:50:28 -0600

    se agrega clase GestorImagen

---
+++ src/herramientas/GestorImagen.java
@@ -0,0 +1,13 @@
+package herramientas;
+
+import java.awt.Image;
+// va orientada a poder obtener una imagen de la pc
+public class GestorImagen {
+
+
+    public Image abrirImagen(){
+
+
+    }
+    
+}
